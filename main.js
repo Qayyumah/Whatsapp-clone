b242cacb0001e4463daa79c9c014a709ca6f29f9
Qayyumah <qayyumatsalman@gmail.com>
2023-11-25 09:47:13 +0100

    My seventh commit

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-document.getElementById('updates').addEventListener('click', function(){
-    document.getElementById('display').innerHTML = 'ppppp'
+document.getElementById('calls').addEventListener('click', function(){
+    document.getElementById('con').innerHTML = document.getElementById('hed').innerHTML
+})
+
+document.getElementById('m-ch').addEventListener('click', function(){
+    document.getElementById('con').innerHTML = document.getElementById('chat-container').innerHTML
 })
