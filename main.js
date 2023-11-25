f70473ab4d41a719f4bb3ee321a1bcdad75b1eb6
Qayyumah <qayyumatsalman@gmail.com>
2023-11-25 11:56:30 +0100

    add status nav

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ document.getElementById('calls').addEventListener('click', function(){
 document.getElementById('m-ch').addEventListener('click', function(){
     document.getElementById('con').innerHTML = document.getElementById('chat-container').innerHTML
 })
+
+document.getElementById('Updates').addEventListener('click', function(){
+    document.getElementById('con').innerHTML = document.getElementById('upd').innerHTML
+})
